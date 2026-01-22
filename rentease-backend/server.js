b8d11340d524ef535486b2f44869ea1247721a8b
Pratismith Gogoi <106908989+Pratismith@users.noreply.github.com>
2026-01-22 20:55:56 +0530

    Update Project2 with 21 Jan version code

--- rentease-backend/server.js
+++ rentease-backend/server.js
@@ -64,7 +64,7 @@ mongoose
   })
   .then(() => {
     console.log("✅ MongoDB Connected");
-    const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 4000;
     app.listen(PORT, "0.0.0.0", () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch((err) => console.error("❌ DB Connection Error:", err));
